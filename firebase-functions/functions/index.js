8d77b7253cb5d30da20dfcdbfbbb4d1ee3283c60
Milan <comiv92@gmail.com>
2020-08-06 17:06:27 +0200

    Adds createScream function

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+const serviceAccount = require('./admin.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://screams-social-app.firebaseio.com"
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,14 +16,33 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello world!!");
 });
 
-exports.getScreams = functions.https.onRequest((request, response) => {
+exports.getScreams = functions.https.onRequest((req, res) => {
   admin.firestore().collection('screams').get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
         screams.push(doc.data());
       });
-      return response.json(screams);
+      return res.json(screams);
     })
     .catch(err => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully!` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+      console.error(err);
+    });
 });
