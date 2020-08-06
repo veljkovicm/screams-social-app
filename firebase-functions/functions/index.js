9c1a7b2ab8a407f3786bb0300d95d4582a279496
Milan <comiv92@gmail.com>
2020-08-06 16:37:46 +0200

    Adds getScreams function

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,4 +9,16 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello world!!");
+});
+
+exports.getScreams = functions.https.onRequest((request, response) => {
+  admin.firestore().collection('screams').get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+      return response.json(screams);
+    })
+    .catch(err => console.error(err));
 });
