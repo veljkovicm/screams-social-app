a2d2c996f3c6a337929331fc2f64b342dd051bc7
Milan <comiv92@gmail.com>
2020-08-07 10:04:04 +0200

    Adds express

---
+++ firebase-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this this the scream body',
+      createdAt: '2020-08-07T07:47:47.105Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -8,31 +8,37 @@ admin.initializeApp({
   databaseURL: "https://screams-social-app.firebaseio.com"
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello world!!");
-});
+const express = require('express');
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
+const app = express();
+
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(screams);
     })
     .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
+app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    // createdAt: admin.firestore.Timestamp.fromDate(new Date()) firebase date
+    createdAt: new Date().toISOString()
   };
 
   admin.firestore()
@@ -46,3 +52,6 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
