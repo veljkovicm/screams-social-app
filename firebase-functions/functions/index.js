f92b4535c02407a7f04d6b5f29d9e216904912aa
Milan <comiv92@gmail.com>
2020-08-07 13:05:09 +0200

    Adds signup validation

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -13,9 +13,10 @@ admin.initializeApp({
 const firebase = require('firebase');
 firebase.initializeApp(config);
 
+const db = admin.firestore();
+
 app.get('/screams', (req, res) => {
-  admin
-    .firestore()
+  db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
@@ -42,7 +43,7 @@ app.post('/scream', (req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin.firestore()
+  db
     .collection('screams')
     .add(newScream)
     .then(doc => {
@@ -64,15 +65,28 @@ app.post('/signup', (req, res) => {
   };
 
   // TODO valdiate data
-
-  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  db.doc(`/users/${newUser.handle}`).get()
+    .then(doc => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'this handle is already taken.' })
+      } else {
+        return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+      }
+    })
     .then(data => {
-      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`});
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.status(201).json({ token });
     })
     .catch(err => {
       console.error(err);
-      return res.status(500).json({ error: err.code, message: err.message });
-    })
+      if(err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already in use'});
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
 });
 
 exports.api = functions.https.onRequest(app);
