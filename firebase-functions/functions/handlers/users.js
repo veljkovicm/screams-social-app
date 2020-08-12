03dbf7a74465bc903ffb2c6d1ad449ef90092b95
Milan <comiv92@gmail.com>
2020-08-12 17:34:43 +0200

    Adds image validation

--- firebase-functions/functions/handlers/users.js
+++ firebase-functions/functions/handlers/users.js
@@ -101,6 +101,10 @@ exports.uploadImage = (req, res) => {
   let imageToBeUploaded = {};
 
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    if(mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submited!' });
+    }
+
     const imageExtension = filename.split('.')[filename.split('.').length -1];
     imageFileName = `${Math.round(Math.random()*100000000)}.${imageExtension}`;
     const filepath = path.join(os.tmpdir(), imageFileName);
