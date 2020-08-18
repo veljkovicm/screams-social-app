866e94066af5d3e9e6c42b632f096b65d3719ba2
Milan <comiv92@gmail.com>
2020-08-18 13:36:42 +0200

    Adds notification functions

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -4,6 +4,7 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
+const{ db } = require('./util/admin');
 
 const {
   getAllScreams,
@@ -41,4 +42,71 @@ app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
+
+exports.deleteNotificationOnUnlike = functions
+  .region('europe-west1')
+  .firestore.document('likes/{id}')
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err)
+        return;
+      });
+  })
+
+exports.createNotificationOnLike = functions.
+  region('europe-west1')
+  .firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`).get()
+      .then(doc => {
+        if(doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          })
+        }
+      })
+      .then(() => {
+        return
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region('europe-west1')
+  .firestore.document('comments/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`).get()
+      .then(doc => {
+        if(doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          })
+        }
+      })
+      .then(() => {
+        return
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
