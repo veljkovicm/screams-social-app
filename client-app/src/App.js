b84c3cf75ff80781782a869464b22dbafdde9cfe
Milan <comiv92@gmail.com>
2020-08-19 14:50:58 +0200

    Adds a color theme

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 // Components
 import Navbar from './components/Navbar';
@@ -11,9 +13,27 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+});
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
         <Navbar />
         <div className="container">
@@ -25,6 +45,7 @@ function App() {
         </div>
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
