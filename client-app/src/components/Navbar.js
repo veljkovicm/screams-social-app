b29cdbc5a34c7dd0280330ffc6f2fb929c1f6982
Milan <comiv92@gmail.com>
2020-08-19 15:30:35 +0200

    Removes duplicate exports

--- client-app/src/components/Navbar.js
+++ client-app/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
 
 // Material UI stuff
 import AppBar from '@material-ui/core/AppBar';
@@ -7,7 +7,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
 
-export class Navbar extends Component {
+class Navbar extends Component {
   render() {
     return (
       <AppBar>
--- client-app/src/pages/signup.js
+++ client-app/src/pages/signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class Signup extends Component {
+class Signup extends Component {
   render() {
     return (
       <div>
