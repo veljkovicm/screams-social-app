17a90a6f83efea39dc83d3a0b6e5154fe3042f9e
Milan <comiv92@gmail.com>
2020-08-19 14:26:31 +0200

    Adds MaterialUI and main nav

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// Components
+import Navbar from './components/Navbar';
+
+
 // Pages
 import Home from './pages/home';
 import Login from './pages/login';
@@ -11,11 +15,14 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
