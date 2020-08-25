54928973b25304eb5d7c91b527bd8612021b6aa9
Milan <comiv92@gmail.com>
2020-08-25 15:28:02 +0200

    Adds the Profile component

--- client-app/src/pages/home.js
+++ client-app/src/pages/home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 import Scream from '../components/Scream';
+import Profile from '../components/Profile';
 
 export class Home extends Component {
   state = {
@@ -28,7 +29,7 @@ export class Home extends Component {
           {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>Profile...</p>
+          <Profile />
         </Grid>
       </Grid>
     )
--- client-app/src/redux/actions/userActions.js
+++ client-app/src/redux/actions/userActions.js
@@ -3,7 +3,8 @@ import {
   SET_ERRORS,
   CLEAR_ERRORS,
   LOADING_UI,
-  SET_UNAUTHENTICATED
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from '../types';
 import axios from 'axios';
 
@@ -31,6 +32,7 @@ export const logoutUser = () => (dispatch) => {
 }
 
 export const getUserData = () => (dispatch) => {
+  dispatch({ type: LOADING_USER });
   axios.get('/user', {
     headers: {
       Authorization: localStorage.getItem('FBIdToken')
