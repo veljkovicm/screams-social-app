cf3d8923d714e98b179f10d5c42ac0582126fe08
Milan <comiv92@gmail.com>
2020-09-02 13:42:18 +0200

    Adds submitComment action and reducer

--- client-app/src/redux/reducers/dataReducer.js
+++ client-app/src/redux/reducers/dataReducer.js
@@ -6,6 +6,7 @@ import {
   DELETE_SCREAM,
   POST_SCREAM,
   SET_SCREAM,
+  SUBMIT_COMMENT,
 } from '../types';
 
 const initialState = {
@@ -57,6 +58,14 @@ export default function(state = initialState, action){
           ...state.screams,
         ]
       }
+    case SUBMIT_COMMENT:
+        return {
+          ...state,
+          scream: {
+            ...state.scream,
+            comments: [action.payload, ...state.scream.comments],
+          }
+        }
     default:
         return state;
   }
