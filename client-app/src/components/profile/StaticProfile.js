375611086d735fbf7feaafcfb0f54130d92a7342
Milan <comiv92@gmail.com>
2020-09-02 14:48:33 +0200

    Adds StaticProfile component

---
+++ client-app/src/components/profile/StaticProfile.js
@@ -0,0 +1,129 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import dayjs from 'dayjs';
+import { Link } from 'react-router-dom';
+
+// Material UI
+import MuiLink from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+// Icons
+import LocationOn from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import SearchIcon from '@material-ui/icons/Search';
+import { Portal } from '@material-ui/core';
+
+
+const styles = (theme) => ({
+  paper: {
+    padding: 20
+  },
+  profile: {
+    '& .image-wrapper': {
+      textAlign: 'center',
+      position: 'relative',
+    },
+    '& .profile-image-select-wrapper': {
+      background: 'rgba(0,0,0,0.3)',
+      position: 'absolute',
+      top: '0',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      width: '100%',
+      height: '100%',
+      maxWidth: '200px',
+      maxHeight: '200px',
+      borderRadius: '50%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      cursor: 'pointer',
+      opacity: '0',
+      transition: 'all linear .3s',
+      '&:hover': {
+        opacity: '1',
+      },
+    },
+    '& .profile-image': {
+      width: 200,
+      height: 200,
+      objectFit: 'cover',
+      maxWidth: '100%',
+      borderRadius: '50%'
+    },
+    '& .profile-details': {
+      textAlign: 'center',
+      '& span, svg': {
+        verticalAlign: 'middle'
+      },
+      '& a': {
+        color: theme.palette.primary.main
+      }
+    },
+    '& hr': {
+      border: 'none',
+      margin: '0 0 10px 0'
+    },
+  },
+
+});
+
+const StaticProfile = (props) => {
+  const {
+    classes,
+    profile: {
+      handle,
+      createdAt,
+      imageUrl,
+      bio,
+      website,
+      location 
+    }
+  } = props;
+
+  return (
+    <Paper className={classes.paper}>
+        <div className={classes.profile}>
+          <div className="image-wrapper">
+            <img src={imageUrl} alt="profile" className="profile-image" />
+          </div>
+          <hr />
+          <div className="profile-details">
+            <MuiLink component={Link} to={`/users/${handle}`} color="primary" variant="h5">
+              @{handle}
+            </MuiLink>
+            <hr />
+            {bio && <Typography variant="body2">{bio}</Typography>}
+            <hr />
+            {location && (
+              <Fragment>
+                <LocationOn color="primary" /> <span>{location}</span>
+                <hr />
+              </Fragment>
+            )}
+            {website && (
+              <Fragment>
+                <LinkIcon color ="primary" />
+                <a href={website} target="_blank" rel="noopener noreferrer">
+                  {'  '}{website}
+                </a>
+                <hr />
+              </Fragment>
+            )}
+            <CalendarToday color="primary" />{'  '}
+            <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
+          </div>
+        </div>
+      </Paper>
+  )
+}
+
+StaticProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(StaticProfile);
