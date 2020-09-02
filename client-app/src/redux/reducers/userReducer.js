c315b6e209b18f9a8297bdb434db02bcd49f4ad5
Milan <comiv92@gmail.com>
2020-09-02 16:42:14 +0200

    Adds notification state action and reducer

--- client-app/src/redux/reducers/userReducer.js
+++ client-app/src/redux/reducers/userReducer.js
@@ -5,6 +5,7 @@ import {
   LOADING_USER,
   LIKE_SCREAM,
   UNLIKE_SCREAM,
+  MARK_NOTIFICATIONS_READ,
 } from '../types';
 
 const initialState = {
@@ -53,6 +54,11 @@ export default function(state = initialState, action){
           (like) => like.screamId !== action.payload.screamId
         )
       }
+    case MARK_NOTIFICATIONS_READ:
+      state.notifications.forEach(notification => notification.read = true);
+      return {
+        ...state
+      };
     default:
       return state;
   }
