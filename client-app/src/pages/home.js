3888d0892fc6d8af9129a4df1a9d9d77fd837491
Milan <comiv92@gmail.com>
2020-09-03 14:55:06 +0200

    Adds skeleton components

--- client-app/src/pages/home.js
+++ client-app/src/pages/home.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import Scream from '../components/scream/Scream';
 import Profile from '../components/profile/Profile';
+import ScreamSkeleton from '../util/ScreamSkeleton';
 
 import { connect } from 'react-redux';
 import { getScreams } from '../redux/actions/dataActions';
@@ -22,7 +23,7 @@ export class Home extends Component {
       screams.map(scream => 
         <Scream key={scream.screamId} scream={scream} />
       )
-    ) : <p>Loading...</p>
+    ) : <ScreamSkeleton />
     return (
       <Grid container spacing={4}>
         <Grid item sm={8} xs={12}>
--- client-app/src/pages/user.js
+++ client-app/src/pages/user.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import Scream from '../components/scream/Scream';
 import StaticProfile from '../components/profile/StaticProfile';
+import ScreamSkeleton from '../util/ScreamSkeleton';
+import ProfileSkeleton from '../util/ProfileSkeleton';
 
 import Grid from '@material-ui/core/Grid' ;
 
@@ -37,7 +39,7 @@ function User(props) {
 
 
   const screamsMarkup = loading ? (
-    <p>Loading data...</p>
+    <ScreamSkeleton />
   ) : screams === null ? (
     <p>No screams from this user</p>
   ) : !screamIdParam ? (
@@ -58,7 +60,7 @@ function User(props) {
       </Grid>
       <Grid item sm={4} xs={12}>
        {profile === null ? (
-         <p>Loading profile...</p>
+         <ProfileSkeleton />
        ) : ( <StaticProfile profile={profile} />) }
       </Grid>
     </Grid>
